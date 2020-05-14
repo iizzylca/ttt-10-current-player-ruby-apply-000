1edb6291955ba752964081c340884d19f2497b16
Israel Canessa <israellcanessa@gmail.com>
2020-05-14 03:31:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |token|
-    if token == "X" || token == "O"
+    if token == "X" || token =="O"
       counter += 1
     end
   end
-  counter
-end
-
-def current_player(board)
-  counter = 0
-  board.each do |token|
-    until token == "X"
-    counter += 1
-    end
-  end
+  counter 
 end
